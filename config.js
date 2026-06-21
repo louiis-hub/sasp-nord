3f8c6d77d8533bc408982e8e4559c9cae56c8ecb
louiis-hub <louis.herrou1@gmail.com>
2026-06-21 05:24:22 +0200

    Configure Apps Script API endpoint

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // Colle ici l'URL de deploiement Web App Apps Script qui finit par /exec.
 // Exemple :
 // window.SASP_API_URL = 'https://script.google.com/macros/s/XXXXXXXX/exec';
-window.SASP_API_URL = '';
+window.SASP_API_URL = 'https://script.google.com/macros/s/AKfycbxg6hAzEAz0HN7aItXRLjuSOcn8DCTN2u45AQ6bZL3g8NKZ5VT7jdvCnG7mGaem4N9_cg/exec';
