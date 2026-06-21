a66fe8688c9b74aa0b1ebbbd0c08b8d12eb4dded
louiis-hub <louis.herrou1@gmail.com>
2026-06-21 05:20:08 +0200

    Connect GitHub frontend to Apps Script API

---
+++ config.js
@@ -0,0 +1,4 @@
+// Colle ici l'URL de deploiement Web App Apps Script qui finit par /exec.
+// Exemple :
+// window.SASP_API_URL = 'https://script.google.com/macros/s/XXXXXXXX/exec';
+window.SASP_API_URL = '';
